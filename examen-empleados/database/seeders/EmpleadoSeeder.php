a2866e2cfb65f4b79730db616e54b89a44c20eaf
Limberth Cruz <abranlopezl121@gmail.com>
2025-10-27 20:42:00 -0400

    feat(api): ejecutar migraciones y seeders BD creada y probada

--- examen-empleados/database/seeders/EmpleadoSeeder.php
+++ examen-empleados/database/seeders/EmpleadoSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Empleado;
-use Illuminate\Support\Str;
+
 
 class EmpleadoSeeder extends Seeder
 {
